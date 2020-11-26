5337a9d62d38f5e5d20f4d3c0d8531f2eda34b25
DanielCabral <daniel-cabral14@hotmail.com>
2020-11-26 11:21:03 -0300

    route delete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const routes=require('./routes.js');
 const cors=require('cors');
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 const app=express();
 
