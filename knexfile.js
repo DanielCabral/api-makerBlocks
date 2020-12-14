a820b424ba8a51458cbdde4813313bccffa82f21
claudeilton dantas <fcd007@hotmail.com>
2020-12-14 10:52:08 -0300

    update core nanode

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,5 @@
 module.exports = {
-
-    // development: {
-    //   client: 'sqlite3',
-    //   connection: {
-    //     filename: './src/database/db.sqlite'
-    //   },
-    //   migrations:{
-    //       directory:"./src/database/migrations"
-    //   },
-    //   useNullAsDefault: true
-    // },
-    development: {
+development: {
       client: 'mysql',
       connection: {
         host: 'us-cdbr-east-02.cleardb.com',
